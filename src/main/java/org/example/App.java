f7280d7217e9f6bee0681affb21a7d94eab6943e
Zimbee <61887028+Zmbeboy@users.noreply.github.com>
2021-09-08 19:37:29 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Eric Gass
+ */
+
+package org.example;
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("What is the password ");
+        String guess = scan.nextLine();
+
+        String password = "wag1fam";
+
+        if(guess.equals(password))
+        {
+            System.out.println("Welcome!");
+        }
+        else
+        {
+            System.out.println("I don't know you");
+        }
+    }
+}
